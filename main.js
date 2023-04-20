b5ca48c64bf9c3c0d88486f0b924497d7aa4ae24
bhavya1254 <114231177+bhavya1254@users.noreply.github.com>
2023-04-20 17:41:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+previous_result = "";
+function setup() {
+  canvas = createCanvas(200, 200);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modalLoaded);
+}
+
+function modalLoaded()
+{
+console.log('Modal Loaded');
+}
+
+function draw()
+{
+  image(video, 0, 0, 200, 200);
+  classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+if(error)
+{
+  console.error(error);
+} else{
+  if((results[0].confidence > 0.5) && (previous_result != results[0].label)){
+    console.log(results);
+    previous_result = results[0].label;
+    var synth = window.speechSynthesis;
+   speak_data = 'Object detected is - '+results[0].label;
+   var utterThis = new SpeechSynthesisUtterance(speak_data);
+   synth.speak(utterThis);
+   
+   document.getElementById("result_object_name").innerHTML = results[0].label;
+   document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
+}
+
